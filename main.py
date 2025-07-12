59143b4a30b58f26aef7f4c95b401a1829fd0572
ajmvader05 <ajmvader05@gmail.com>
2025-07-12 11:56:14 -0400

    Create inital main.py script with varibles and print statements

---
+++ main.py
@@ -0,0 +1,8 @@
+# comment - helps others understand the code
+project_name = "Kali Academic Search"
+phase = 1
+
+print("Project Name:", project_name)
+print("Current Phase:", phase)
+print("---")
+print("Initalization complete.")
