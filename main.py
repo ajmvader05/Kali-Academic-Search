7a4f85762bc898addb56248789a3c15df1008863
ajmvader05 <ajmvader05@gmail.com>
2025-07-12 12:18:09 -0400

    Add engine list and for loop to iterate through it

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ phase = 1
 print("Project Name:", project_name)
 print("Current Phase:", phase)
 print("---")
+# list of strings
+academic_engines = ["Google Scholar", "arXiv.org", "JSTOR", "PubMed Central"]
+
+print("Target Academic Engines:")
+for engine in academic_engines:
+	print("-", engine)
+print("---")
 print("Initalization complete.")
 
-# --- New Control Flow Section ---
-print("Checking project status...")
-
-if phase == 1:
-	print ("Status: We are in the foundational skills phase.")
-else:
-	print("Status: We have moved beyond the basics.")
