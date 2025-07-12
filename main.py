9be27ad1f3e897797a205df0574d5ccb40015a7d
ajmvader05 <ajmvader05@gmail.com>
2025-07-12 12:07:25 -0400

    Added logic to check project phase

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ print("Project Name:", project_name)
 print("Current Phase:", phase)
 print("---")
 print("Initalization complete.")
+
+# --- New Control Flow Section ---
+print("Checking project status...")
+
+if phase == 1:
+	print ("Status: We are in the foundational skills phase.")
+else:
+	print("Status: We have moved beyond the basics.")
